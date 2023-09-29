4a98fe6e184df134858aa6547d14cc7d8fa4ee9e
R7STAR <141929043+R7STAR@users.noreply.github.com>
2023-09-29 22:22:50 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5247304559").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/romeoraja1/Romeo_Music")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/R7STAR/Romeo_Music")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
